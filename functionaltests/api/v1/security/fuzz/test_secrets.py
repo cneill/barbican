ea759bb72902693975db176bb53d7d2b3c1a9b86
Charles Neill <charles.neill@rackspace.com>
2015-02-10 13:40:53 -0600

    security tests, first pass

---
+++ functionaltests/api/v1/security/fuzz/test_secrets.py
@@ -0,0 +1,285 @@
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import base64
+import binascii
+import json
+import sys
+import time
+
+from testtools import testcase
+
+from barbican.tests import utils
+from functionaltests.api import base
+from functionaltests.api.v1.behaviors import secret_behaviors
+from functionaltests.api.v1.models import secret_models
+from functionaltests.api.v1.security import security_utils
+
+# TODO(tdink) Move to a config file
+secret_create_defaults_data = {
+    "name": "AES key",
+    "expiration": "2018-02-28T19:14:44.180394",
+    "algorithm": "aes",
+    "bit_length": 256,
+    "mode": "cbc",
+    "payload": "gF6+lLoF3ohA9aPRpt+6bQ==",
+    "payload_content_type": "application/octet-stream",
+    "payload_content_encoding": "base64",
+}
+
+secret_create_nones_data = {
+    "name": None,
+    "expiration": None,
+    "algorithm": None,
+    "bit_length": None,
+    "mode": None,
+    "payload": None,
+    "payload_content_type": None,
+    "payload_content_encoding": None,
+}
+
+secret_create_emptystrings_data = {
+    "name": '',
+    "expiration": '',
+    "algorithm": '',
+    "bit_length": '',
+    "mode": '',
+    "payload": '',
+    "payload_content_type": '',
+    "payload_content_encoding": '',
+}
+
+secret_create_two_phase_data = {
+    "name": "AES key",
+    "expiration": "2018-02-28T19:14:44.180394",
+    "algorithm": "aes",
+    "bit_length": 256,
+    "mode": "cbc",
+}
+
+secret_create_all_the_things = {
+    'name': '',
+    'expiration': '',
+    'algorithm': '',
+    'bit_length': '',
+    'mode': '',
+    'payload_content_type': '',
+    'payload': '',
+    'content_types': '',
+    'payload_content_encoding': '',
+    'secret_ref': '',
+    'status': '',
+    'updated': '',
+    'created': ''
+}
+
+
+@utils.parameterized_test_case
+class SecretsTestCase(base.TestCase):
+
+    def setUp(self):
+        super(SecretsTestCase, self).setUp()
+        self.behaviors = secret_behaviors.SecretBehaviors(self.client)
+
+    def tearDown(self):
+        self.behaviors.delete_all_created_secrets()
+        super(SecretsTestCase, self).tearDown()
+
+    '''
+    @testcase.attr('security')
+    def test_fuzz_create_secret_all_single(self):
+        """Create a secret with 1 param as fuzzstring, rest undefined
+        SLOOOOOOOOW
+
+        Should return 400 ?"""
+        models = security_utils.fuzz_model(
+            secret_models.SecretModel,
+            skeleton=secret_create_defaults_data,
+            fuzz_string_type='all',
+            fuzz_type='all')
+
+        for model in models:
+            resp, secret_ref = self.behaviors.create_secret(model)
+            # self.assertEqual(resp.status_code, 400)
+            assert(resp.status_code in [400, 201])
+    @testcase.attr('security')
+    def test_fuzz_create_secret_all_all(self):
+        """Create a secret with all params as fuzzstring, rest undefined
+
+        Should return 400 ?"""
+        models = security_utils.fuzz_model(
+            secret_models.SecretModel,
+            skeleton=secret_create_all_the_things,
+            fuzz_string_type='all',
+            fuzz_type='all')
+
+        for model in models:
+            resp, secret_ref = self.behaviors.create_secret(model)
+            # self.assertEqual(resp.status_code, 400)
+            assert(resp.status_code in [400, 201])
+    @testcase.attr('security')
+    def test_fuzz_create_secret_unicode_single(self):
+        """Create a secret with all params as a unicode char, rest undefined
+        SLOOOOOOOOOOOOOOOOOOOOOOW
+
+        Should return 400 ?"""
+        models = security_utils.fuzz_model(
+            secret_models.SecretModel,
+            # skeleton=secret_create_all_the_things,
+            skeleton=secret_create_defaults_data,
+            fuzz_string_type='unicode',
+            fuzz_type='single')
+
+        for model in models:
+            resp, secret_ref = self.behaviors.create_secret(model)
+            # self.assertEqual(resp.status_code, 400)
+            assert(resp.status_code in [400, 201])
+
+    @utils.parameterized_dataset({
+        'unicode': [security_utils.get_fuzz_strings('unicode')],
+        'ascii': [security_utils.get_fuzz_strings('ascii')]
+    })
+    @testcase.attr('negative')
+    def test_secret_create_defaults_invalid_payload(self, payloads):
+        """Covers creating secrets with various invalid payloads."""
+        for payload in payloads:
+            model = secret_models.SecretModel(**secret_create_defaults_data)
+            overrides = {"payload_content_type": "application/octet-stream",
+                         "payload_content_encoding": "base64",
+                         "payload": payload}
+            model.override_values(**overrides)
+
+            resp, secret_ref = self.behaviors.create_secret(model)
+            self.assertIn(resp.status_code, [400, 201])
+
+    @utils.parameterized_dataset({
+        'unicode': [security_utils.get_fuzz_strings('unicode')],
+        'ascii': [security_utils.get_fuzz_strings('ascii')]
+    })
+    @testcase.attr('negative')
+    def test_fuzz_project_id(self, payloads):
+        """Fuzzes Project-Id Header with random bytes"""
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+
+        for payload in payloads:
+            if payload.strip():
+                headers = {'X-Project-Id': payload.encode('utf8'),
+                           'X-Auth-Token': self.client._auth.token}
+
+            resp = self.client.post('secrets', request_model=model,
+                                    use_auth=False, extra_headers=headers)
+
+            self.assertIn(resp.status_code, [400, 201])
+
+    @utils.parameterized_dataset({
+        'unicode': [security_utils.get_fuzz_strings('unicode')],
+        'ascii': [security_utils.get_fuzz_strings('ascii')]
+    })
+    @testcase.attr('negative')
+    def test_fuzz_content_type(self, payloads):
+        """Fuzzes Content-Type Header with random bytes
+
+        Should return 415"""
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+
+        for payload in payloads:
+            if payload.strip():
+                headers = {'Content-Type': payload}
+
+            resp = self.client.post('secrets', request_model=model,
+                                    extra_headers=headers)
+
+            self.assertEqual(resp.status_code, 415)
+
+    @utils.parameterized_dataset({
+        # 'unicode': [security_utils.get_fuzz_strings('unicode')],
+        'ascii': [security_utils.get_fuzz_strings('ascii')]
+    })
+    @testcase.attr('negative')
+    def test_fuzz_cookie_header(self, payloads):
+        """Fuzzes Cookie header w/ random bytes
+
+        Should return 201"""
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+
+        for payload in payloads:
+            if payload.strip():
+                headers = {'Cookie': payload}
+
+                resp = self.client.post('secrets', request_model=model,
+                                        extra_headers=headers)
+
+                self.assertEqual(resp.status_code, 201)
+
+    @utils.parameterized_dataset({
+        # 'unicode': [security_utils.get_fuzz_strings('unicode')],
+        'ascii': [security_utils.get_fuzz_strings('ascii')]
+    })
+    @testcase.attr('negative')
+    def test_fuzz_accept_header(self, payloads):
+        """Fuzzes Accept header w/ random bytes
+
+        Should return 406"""
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+
+        for payload in payloads:
+            if payload.strip():
+                headers = {'Accept': payload}
+
+                resp = self.client.post('secrets', request_model=model,
+                                        extra_headers=headers)
+
+                self.assertEqual(resp.status_code, 406)
+    '''
+
+    @utils.parameterized_dataset({
+        'accept': {'header': 'Accept',
+                   'payloads': security_utils.get_fuzz_strings('ascii')},
+        'cookie': {'header': 'Cookie',
+                   'payloads': security_utils.get_fuzz_strings('ascii')},
+        'host': {'header': 'Host',
+                 'payloads': security_utils.get_fuzz_strings('ascii')},
+        'content_type': {'header': 'Content-Type',
+                         'payloads': security_utils.get_fuzz_strings('ascii')},
+        'accept_enc': {'header': 'Accept-Encoding',
+                       'payloads': security_utils.get_fuzz_strings('ascii')},
+        'user_agent': {'header': 'User-Agent',
+                       'payloads': security_utils.get_fuzz_strings('ascii')},
+        'connection': {'header': 'Connection',
+                       'payloads': security_utils.get_fuzz_strings('ascii')},
+
+    })
+    @testcase.attr('negative')
+    def test_fuzz_authed_header(self, **kwargs):
+        """Fuzzes all headers w/ random bytes
+
+        Should return [201, 406, 415]"""
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+
+        for payload in kwargs['payloads']:
+            if payload.strip():
+                headers = {kwargs['header']: payload}
+
+                resp = self.client.post('secrets', request_model=model,
+                                        extra_headers=headers)
+                if kwargs['header'] == 'Content-Type':
+                    self.assertEqual(resp.status_code, 415)
+                elif kwargs['header'] == 'Host':
+                    self.assertEqual(resp.status_code, 200)
+                    print resp.headers['location']
+                elif kwargs['header'] == 'Accept':
+                    self.assertEqual(resp.status_code, 406)
+                else:
+                    self.assertIn(resp.status_code, [201, 406, 415])
---
+++ functionaltests/api/v1/security/test_secrets.py
@@ -0,0 +1,330 @@
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import base64
+import binascii
+import json
+import sys
+import time
+
+from testtools import testcase
+
+from barbican.tests import utils
+from functionaltests.api import base
+from functionaltests.api.v1.behaviors import secret_behaviors
+from functionaltests.api.v1.models import secret_models
+from functionaltests.api.v1.security import security_utils
+
+# TODO(tdink) Move to a config file
+secret_create_defaults_data = {
+    "name": "AES key",
+    "expiration": "2018-02-28T19:14:44.180394",
+    "algorithm": "aes",
+    "bit_length": 256,
+    "mode": "cbc",
+    "payload": "gF6+lLoF3ohA9aPRpt+6bQ==",
+    "payload_content_type": "application/octet-stream",
+    "payload_content_encoding": "base64",
+}
+
+secret_create_nones_data = {
+    "name": None,
+    "expiration": None,
+    "algorithm": None,
+    "bit_length": None,
+    "mode": None,
+    "payload": None,
+    "payload_content_type": None,
+    "payload_content_encoding": None,
+}
+
+secret_create_emptystrings_data = {
+    "name": '',
+    "expiration": '',
+    "algorithm": '',
+    "bit_length": '',
+    "mode": '',
+    "payload": '',
+    "payload_content_type": '',
+    "payload_content_encoding": '',
+}
+
+secret_create_two_phase_data = {
+    "name": "AES key",
+    "expiration": "2018-02-28T19:14:44.180394",
+    "algorithm": "aes",
+    "bit_length": 256,
+    "mode": "cbc",
+}
+
+
+@utils.parameterized_test_case
+class SecretsTestCase(base.TestCase):
+
+    def setUp(self):
+        super(SecretsTestCase, self).setUp()
+        self.behaviors = secret_behaviors.SecretBehaviors(self.client)
+
+    def tearDown(self):
+        self.behaviors.delete_all_created_secrets()
+        super(SecretsTestCase, self).tearDown()
+
+    # OVERSIZED REQUESTS #
+    @testcase.attr('security')
+    def test_huge_bogus_name(self):
+        """Create a secret with a name larger than the max field size
+
+        Should return 413(?)"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'name': 'a' * (self.max_field_size + 1)}
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 400)
+
+    @testcase.attr('security')
+    def test_huge_bogus_algorithm(self):
+        """Create a secret with a bogus algorithm larger than the max field size
+
+        Should return 400"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'algorithm': 'a' * (self.max_field_size + 1)}
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 400)
+
+    @testcase.attr('security')
+    def test_bogus_encoding(self):
+        """Create a secret with a bogus encoding, larger than the max field size
+
+        Should return 400"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'payload_content_encoding': 'a' * (self.max_field_size
+                                                        + 1)}
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 400)
+
+    @testcase.attr('security')
+    def test_huge_bogus_secret(self):
+        """Create a secret with a bogus secret, larger than the max field size
+
+        Should return 413"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        # overrides = {'payload': 'a' * (self.max_payload_size + 1)}
+        overrides = {'payload': 'a' * 100000}
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 413)
+
+    @testcase.attr('security')
+    def test_huge_bogus_project_id(self):
+        """Create a secret with a bogus 1500-character project ID
+
+        Should return 201"""
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+        headers = {'X-Project-Id': 'a' * 1500,
+                   'X-Auth-Token': self.client._auth.token}
+
+        resp = self.client.post('secrets', request_model=model,
+                                use_auth=False, extra_headers=headers)
+
+        self.assertEqual(resp.status_code, 201)
+
+    @testcase.attr('security')
+    def test_huge_bogus_token(self):
+        """Create a secret with a bogus 3500-character token
+
+        Should return 413"""
+
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+        headers = {'X-Auth-Token': 'a' * 3500}
+
+        resp = self.client.post('secrets', request_model=model,
+                                use_auth=False, extra_headers=headers)
+
+        self.assertEqual(resp.status_code, 401)
+
+    '''
+    @testcase.attr('security')
+    def test_huge_xss_encoding(self):
+        """Create a secret with XSS encoding, larger than the max field size
+
+        Should return 413"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'payload_content_encoding':
+                     '\\x27\u0027\'"><script>alert(1)</script>' * 100000}
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 413)
+
+    @testcase.attr('security')
+    def test_huger_encoding(self):
+        """Create a secret with XSS encoding, larger than the max field size
+
+        Should return 413"""
+        # RETURNS 400, INVALID JSON - CAN'T READ IT ALL
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'payload_content_encoding': 'a' * 10000000}
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 413)
+    '''
+
+    # CONTENT TYPES #
+    @testcase.attr('security')
+    def test_atom_content_type(self):
+        """Create a secret, with atom+xml as content type
+
+        Should return 415"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        headers = {'Content-Type': 'application/atom+xml'}
+        resp, secret_ref = self.behaviors.create_secret(test_model, headers)
+        self.assertEqual(resp.status_code, 415)
+
+    # BAD CONTENT #
+    @testcase.attr('security')
+    def test_xss_secret(self):
+        """Create a secret with XSS
+
+        Should return 400"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'payload': '<script>alert(1)</script>',
+                     "payload_content_type": "text/plain"}
+
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 400)
+
+    @testcase.attr('security')
+    def test_sqli_secret(self):
+        """Create a secret with SQLi
+
+        Should return 400"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'payload': '\'" union all select * from users/*--',
+                     "payload_content_type": "text/plain"}
+
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 400)
+
+    @testcase.attr('security')
+    def test_utf8_secret(self):
+        """Create a secret with unicode
+
+        Should return 400"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'payload': unichr(255) + unichr(0),
+                     "payload_content_type": "text/plain"}
+
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 400)
+
+    @testcase.attr('security')
+    def test_b64_xss_secret(self):
+        """Create a secret with XSS
+
+        Should return 400"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'payload': 'PHNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg==',
+                     "payload_content_type": "text/plain"}
+
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 400)
+
+    @testcase.attr('security')
+    def test_b64_sqli_secret(self):
+        """Create a secret with SQLi
+
+        Should return 400"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'payload': 'JyIgdW5pb24gYWxsIHNlbGVjdCAqIGZyb20gdXNlcnMvKi0t',
+                     "payload_content_type": "text/plain"}
+
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 400)
+
+    @testcase.attr('security')
+    def test_b64_utf8_secret(self):
+        """Create a secret with UTF8
+
+        Should return 400"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {'payload': '/w==',
+                     "payload_content_type": "text/plain"}
+
+        test_model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(test_model)
+        self.assertEqual(resp.status_code, 400)
+
+    # UNAUTHED TESTS #
+    @testcase.attr('security')
+    def test_unauthed_create(self):
+        """Create a secret without a token or Project-Id
+
+        Should return 401"""
+        test_model = secret_models.SecretModel(**secret_create_defaults_data)
+
+        resp = self.client.post('secrets', request_model=test_model,
+                                use_auth=False)
+
+        self.assertEqual(resp.status_code, 401)
+
+    @testcase.attr('security')
+    def test_unauthed_get(self):
+        """Attempt to read a secret without a token or Project-Id
+
+        Should return 401"""
+        # test_model = secret_models.SecretModel(**secret_create_defaults_data)
+        secret_ref = 'http://localhost:9311/v1/secrets/'
+        secret_ref += '91ff8f86-677c-428c-bda6-1b61db872add'
+
+        headers = {'Accept': '*/*',
+                   'Accept-Encoding': '*/*'}
+
+        resp = self.client.get(secret_ref, extra_headers=headers,
+                               use_auth=False)
+
+        self.assertEqual(resp.status_code, 401)
+
+    @testcase.attr('security')
+    def test_unauthed_update(self):
+        """Attempt to update a secret without a token or Project-Id
+
+        Should return 401"""
+        secret_ref = 'http://localhost:9311/v1/secrets/'
+        secret_ref += '91ff8f86-677c-428c-bda6-1b61db872add'
+        headers = {'Content-Type': 'text/plain',
+                   'Content-Encoding': 'base64'}
+
+        resp = self.client.put(secret_ref, data=None, extra_headers=headers,
+                               use_auth=False)
+
+        self.assertEqual(resp.status_code, 401)
+
+    @testcase.attr('security')
+    def test_unauthed_delete(self):
+        """Attempt to delete a secret without a token or Project-Id
+
+        Should return 401"""
+        secret_ref = 'http://localhost:9311/v1/secrets/'
+        secret_ref += '91ff8f86-677c-428c-bda6-1b61db872add'
+
+        resp = self.client.delete(secret_ref, use_auth=False)
+
+        self.assertEqual(resp.status_code, 401)
