b3b262bcd07687d700bb7c088fb3bba1584854e5
Charles Neill <charles.neill@rackspace.com>
2015-03-02 16:55:17 -0600

    refactoring

--- functionaltests/api/v1/security/test_secrets.py
+++ functionaltests/api/v1/security/test_secrets.py
@@ -83,21 +83,23 @@ class SecretsTestCase(base.TestCase):
         self.client2 = client.BarbicanClient(auth_provider)
         self.behaviors2 = secret_behaviors.SecretBehaviors(self.client2)
 
-
     def tearDown(self):
         self.behaviors.delete_all_created_secrets()
         super(SecretsTestCase, self).tearDown()
 
     # OVERSIZED REQUESTS #
+
     @testcase.attr('security')
     def test_huge_bogus_name(self):
         """Create a secret with a name larger than the max field size
 
         Should return 413(?)"""
-        test_model = secret_models.SecretModel(**secret_create_defaults_data)
-        overrides = {'name': 'a' * (self.max_field_size + 1)}
-        test_model.override_values(**overrides)
-        resp, secret_ref = self.behaviors.create_secret(test_model)
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {
+            'name': 'a' * (self.max_field_size + 1)
+        }
+        model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(model)
         self.assertEqual(resp.status_code, 400)
 
     @testcase.attr('security')
@@ -105,10 +107,12 @@ class SecretsTestCase(base.TestCase):
         """Create a secret with a bogus algorithm larger than the max field size
 
         Should return 400"""
-        test_model = secret_models.SecretModel(**secret_create_defaults_data)
-        overrides = {'algorithm': 'a' * (self.max_field_size + 1)}
-        test_model.override_values(**overrides)
-        resp, secret_ref = self.behaviors.create_secret(test_model)
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {
+            'algorithm': 'a' * (self.max_field_size + 1)
+        }
+        model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(model)
         self.assertEqual(resp.status_code, 400)
 
     @testcase.attr('security')
@@ -116,11 +120,12 @@ class SecretsTestCase(base.TestCase):
         """Create a secret with a bogus encoding, larger than the max field size
 
         Should return 400"""
-        test_model = secret_models.SecretModel(**secret_create_defaults_data)
-        overrides = {'payload_content_encoding': 'a' * (self.max_field_size
-                                                        + 1)}
-        test_model.override_values(**overrides)
-        resp, secret_ref = self.behaviors.create_secret(test_model)
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {
+            'payload_content_encoding': 'a' * (self.max_field_size + 1)
+        }
+        model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(model)
         self.assertEqual(resp.status_code, 400)
 
     @testcase.attr('security')
@@ -128,11 +133,12 @@ class SecretsTestCase(base.TestCase):
         """Create a secret with a bogus secret, larger than the max field size
 
         Should return 413"""
-        test_model = secret_models.SecretModel(**secret_create_defaults_data)
-        # overrides = {'payload': 'a' * (self.max_payload_size + 1)}
-        overrides = {'payload': 'a' * 100000}
-        test_model.override_values(**overrides)
-        resp, secret_ref = self.behaviors.create_secret(test_model)
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+        overrides = {
+            'payload': 'a' * 100000
+        }
+        model.override_values(**overrides)
+        resp, secret_ref = self.behaviors.create_secret(model)
         self.assertEqual(resp.status_code, 413)
 
     @testcase.attr('security')
@@ -141,97 +147,89 @@ class SecretsTestCase(base.TestCase):
 
         Should return 201"""
         model = secret_models.SecretModel(**secret_create_defaults_data)
-        headers = {'X-Project-Id': 'a' * 1500,
-                   'X-Auth-Token': self.client._auth.token}
-
-        resp = self.client.post('secrets', request_model=model,
-                                use_auth=False, extra_headers=headers)
-
+        headers = {
+            'X-Project-Id': 'a' * 1500,
+            'X-Auth-Token': self.client._auth.token
+        }
+        resp = self.client.post(
+            'secrets', request_model=model, use_auth=False,
+            extra_headers=headers
+        )
         self.assertEqual(resp.status_code, 201)
 
-    '''
-    @testcase.attr('security')
-    def test_huger_encoding(self):
-        """Create a secret with XSS encoding, larger than the max field size
-
-        Should return 413"""
-        # RETURNS 400, INVALID JSON - CAN'T READ IT ALL
-        test_model = secret_models.SecretModel(**secret_create_defaults_data)
-        overrides = {'payload_content_encoding': 'a' * 10000000}
-        test_model.override_values(**overrides)
-        resp, secret_ref = self.behaviors.create_secret(test_model)
-        self.assertEqual(resp.status_code, 413)
-    '''
-
     # CONTENT TYPES #
-    @utils.parameterized_dataset(
-        {'atom_xml': ['application/atom+xml'],
-         'app_xml': ['application/xml'],
-         'txt_xml': ['text/xml'],
-         'app_soap_xml': ['application/soap+xml'],
-         'app_rdf_xml': ['application/rdf+xml'],
-         'app_rss_xml': ['application/rss+xml'],
-         'app_js': ['application/javascript'],
-         'app_ecma': ['application/ecmascript'],
-         'app_x_js': ['application/x-javascript'],
-         'txt_js': ['text/javascript'],
-         'multipart_enc': ['multipart/encrypted'],
-         'multipart_form': ['multipart/form-data'],
-         'app_form': ['application/x-www-form-urlencoded'],
-         'app_pkcs12': ['application/x-pkcs12'],
-         'msg_http': ['message/http'],
-         'msg_partial': ['message/partial'],
-         'example': ['example']
-         })
+
+    @utils.parameterized_dataset({
+        'atom_xml': ['application/atom+xml'],
+        'app_xml': ['application/xml'],
+        'txt_xml': ['text/xml'],
+        'app_soap_xml': ['application/soap+xml'],
+        'app_rdf_xml': ['application/rdf+xml'],
+        'app_rss_xml': ['application/rss+xml'],
+        'app_js': ['application/javascript'],
+        'app_ecma': ['application/ecmascript'],
+        'app_x_js': ['application/x-javascript'],
+        'txt_js': ['text/javascript'],
+        'multipart_enc': ['multipart/encrypted'],
+        'multipart_form': ['multipart/form-data'],
+        'app_form': ['application/x-www-form-urlencoded'],
+        'app_pkcs12': ['application/x-pkcs12'],
+        'msg_http': ['message/http'],
+        'msg_partial': ['message/partial'],
+        'example': ['example']
+    })
     @testcase.attr('security')
     def test_content_type(self, payload):
         """Create a secret with different content types
 
         Should return 415"""
-        test_model = secret_models.SecretModel(**secret_create_defaults_data)
-        headers = {'Content-Type': payload}
-        resp, secret_ref = self.behaviors.create_secret(test_model, headers)
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+        headers = {
+            'Content-Type': payload
+        }
+        resp, secret_ref = self.behaviors.create_secret(model, headers)
         self.assertEqual(resp.status_code, 415)
 
     # BAD CONTENT #
 
-    @utils.parameterized_dataset(
-        {'xss': ['<script>alert(1)</script>'],
-         'sqli': ['\'" union all select * from users/*--'],
-         'unicode': [unichr(255) + unichr(0)],
-         'b64_xss': ['PHNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg=='],
-         'b64_sqli': ['JyIgdW5pb24gYWxsIHNlbGVjdCAqIGZyb20gdXNlcnMvKi0t'],
-         'b64_unicode': ['/w==']})
+    @utils.parameterized_dataset({
+        'xss': ['<script>alert(1)</script>'],
+        'sqli': ['\'" union all select * from users/*--'],
+        'unicode': [unichr(255) + unichr(0)],
+        'b64_xss': ['PHNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg=='],
+        'b64_sqli': ['JyIgdW5pb24gYWxsIHNlbGVjdCAqIGZyb20gdXNlcnMvKi0t'],
+        'b64_unicode': ['/w==']
+    })
     @testcase.attr('negative')
     def test_evil_payload(self, payload):
         """Creates a secret with various evil payloads
 
         Should return 201"""
         model = secret_models.SecretModel(**secret_create_defaults_data)
-
-        overrides = {'payload': payload}
+        overrides = {
+            'payload': payload
+        }
         model.override_values(**overrides)
-
         resp, secret_ref = self.behaviors.create_secret(model)
         self.assertEqual(resp.status_code, 201)
 
-    @utils.parameterized_dataset(
-        {'nullbyte': [chr(0)],
-         'date_w_null': ['2018-02-28T19:14:44.180394' + chr(0)],
-         'date_w_unicode': ['2018-02-28T19:14:44.180394' + unichr(255)],
-         'date_w_format': ['2018-02-28T19:%f14:44.180394'],
-         'huge': ['2018-02-28T12:12:12.' + ('4' * 100000)],
-         })
+    @utils.parameterized_dataset({
+        'nullbyte': [chr(0)],
+        'date_w_null': ['2018-02-28T19:14:44.180394' + chr(0)],
+        'date_w_unicode': ['2018-02-28T19:14:44.180394' + unichr(255)],
+        'date_w_format': ['2018-02-28T19:%f14:44.180394'],
+        'huge': ['2018-02-28T12:12:12.' + ('4' * 100000)],
+    })
     @testcase.attr('negative')
     def test_evil_expiration(self, payload):
-        """Creates a secret with various evil expirations
+        """Creates a secret with various evil expiration times
 
         Should return 400"""
         model = secret_models.SecretModel(**secret_create_defaults_data)
-
-        overrides = {'expiration': payload}
+        overrides = {
+            'expiration': payload
+        }
         model.override_values(**overrides)
-
         resp, secret_ref = self.behaviors.create_secret(model)
         self.assertEqual(resp.status_code, 400)
 
@@ -244,11 +242,13 @@ class SecretsTestCase(base.TestCase):
         Should return 401"""
 
         model = secret_models.SecretModel(**secret_create_defaults_data)
-        headers = {'X-Auth-Token': 'a' * 3500}
-
-        resp = self.client.post('secrets', request_model=model,
-                                use_auth=False, extra_headers=headers)
-
+        headers = {
+            'X-Auth-Token': 'a' * 3500
+        }
+        resp = self.client.post(
+            'secrets', request_model=model, use_auth=False,
+            extra_headers=headers
+        )
         self.assertEqual(resp.status_code, 401)
 
     @testcase.attr('security')
@@ -256,11 +256,10 @@ class SecretsTestCase(base.TestCase):
         """Create a secret without a token or Project-Id
 
         Should return 401"""
-        test_model = secret_models.SecretModel(**secret_create_defaults_data)
-
-        resp = self.client.post('secrets', request_model=test_model,
-                                use_auth=False)
-
+        model = secret_models.SecretModel(**secret_create_defaults_data)
+        resp = self.client.post(
+            'secrets', request_model=model, use_auth=False
+        )
         self.assertEqual(resp.status_code, 401)
 
     @testcase.attr('security')
@@ -268,12 +267,13 @@ class SecretsTestCase(base.TestCase):
         """Attempt to read a secret without a token or Project-Id
 
         Should return 401"""
-        headers = {'Accept': '*/*',
-                   'Accept-Encoding': '*/*'}
-
-        resp = self.client.get(bogus_secret_ref, extra_headers=headers,
-                               use_auth=False)
-
+        headers = {
+            'Accept': '*/*',
+            'Accept-Encoding': '*/*'
+        }
+        resp = self.client.get(
+            bogus_secret_ref, extra_headers=headers, use_auth=False
+        )
         self.assertEqual(resp.status_code, 401)
 
     @testcase.attr('security')
@@ -281,12 +281,13 @@ class SecretsTestCase(base.TestCase):
         """Attempt to update a secret without a token or Project-Id
 
         Should return 401"""
-        headers = {'Content-Type': 'text/plain',
-                   'Content-Encoding': 'base64'}
-
-        resp = self.client.put(bogus_secret_ref, data=None,
-                               extra_headers=headers, use_auth=False)
-
+        headers = {
+            'Content-Type': 'text/plain',
+            'Content-Encoding': 'base64'
+        }
+        resp = self.client.put(
+            bogus_secret_ref, data=None, extra_headers=headers, use_auth=False
+        )
         self.assertEqual(resp.status_code, 401)
 
     @testcase.attr('security')
@@ -296,7 +297,6 @@ class SecretsTestCase(base.TestCase):
         Should return 401"""
 
         resp = self.client.delete(bogus_secret_ref, use_auth=False)
-
         self.assertEqual(resp.status_code, 401)
 
     @testcase.attr('security')
@@ -306,12 +306,14 @@ class SecretsTestCase(base.TestCase):
         Should return 401"""
 
         model = secret_models.SecretModel(**secret_create_defaults_data)
-        headers = {'X-Auth-Token': 'a' * 3500,
-                   'X-Project-Id': bogus_project_id}
-
-        resp = self.client.post('secrets', request_model=model,
-                                use_auth=False, extra_headers=headers)
-
+        headers = {
+            'X-Auth-Token': 'a' * 3500,
+            'X-Project-Id': bogus_project_id
+        }
+        resp = self.client.post(
+            'secrets', request_model=model, use_auth=False,
+            extra_headers=headers
+        )
         self.assertEqual(resp.status_code, 401)
 
     @testcase.attr('security')
@@ -320,11 +322,13 @@ class SecretsTestCase(base.TestCase):
 
         Should return 401"""
         model = secret_models.SecretModel(**secret_create_defaults_data)
-        headers = {'X-Project-Id': bogus_project_id}
-
-        resp = self.client.post('secrets', request_model=model,
-                                use_auth=False, extra_headers=headers)
-
+        headers = {
+            'X-Project-Id': bogus_project_id
+        }
+        resp = self.client.post(
+            'secrets', request_model=model, use_auth=False,
+            extra_headers=headers
+        )
         self.assertEqual(resp.status_code, 401)
 
     @testcase.attr('security')
@@ -332,13 +336,14 @@ class SecretsTestCase(base.TestCase):
         """Attempt to read a secret without a token or Project-Id
 
         Should return 401"""
-        headers = {'Accept': '*/*',
-                   'Accept-Encoding': '*/*',
-                   'X-Project-Id': bogus_project_id}
-
-        resp = self.client.get(bogus_secret_ref, extra_headers=headers,
-                               use_auth=False)
-
+        headers = {
+            'Accept': '*/*',
+            'Accept-Encoding': '*/*',
+            'X-Project-Id': bogus_project_id
+        }
+        resp = self.client.get(
+            bogus_secret_ref, extra_headers=headers, use_auth=False
+        )
         self.assertEqual(resp.status_code, 401)
 
     @testcase.attr('security')
@@ -346,13 +351,14 @@ class SecretsTestCase(base.TestCase):
         """Attempt to update a secret without a token or Project-Id
 
         Should return 401"""
-        headers = {'Content-Type': 'text/plain',
-                   'Content-Encoding': 'base64',
-                   'X-Project-Id': bogus_project_id}
-
-        resp = self.client.put(bogus_secret_ref, data=None,
-                               extra_headers=headers, use_auth=False)
-
+        headers = {
+            'Content-Type': 'text/plain',
+            'Content-Encoding': 'base64',
+            'X-Project-Id': bogus_project_id
+        }
+        resp = self.client.put(
+            bogus_secret_ref, data=None, extra_headers=headers, use_auth=False
+        )
         self.assertEqual(resp.status_code, 401)
 
     @testcase.attr('security')
@@ -360,49 +366,57 @@ class SecretsTestCase(base.TestCase):
         """Attempt to delete a secret without a token or Project-Id
 
         Should return 401"""
-        headers = {'X-Project-Id': bogus_project_id}
-
-        resp = self.client.delete(bogus_secret_ref, use_auth=False,
-                                  extra_headers=headers)
-
+        headers = {
+            'X-Project-Id': bogus_project_id
+        }
+        resp = self.client.delete(
+            bogus_secret_ref, use_auth=False, extra_headers=headers
+        )
         self.assertEqual(resp.status_code, 401)
 
     # LOGIC #
 
     @testcase.attr('security')
     def test_cross_user_read(self):
-        """Attempt to create a secret with one user, and read it with another.
+        """Attempt to create a secret with one admin user, and read it with
+        another.
 
-        RETURNS 200 - CAN SHARE ACROSS USERS
-        Should return 401"""
+        Should return 200 (for now, 401 in future?)"""
 
         model = secret_models.SecretModel(**secret_create_defaults_data)
-        overrides = {'name': 'This is a super secret secret'}
+        overrides = {
+            'name': 'This is a super secret secret'
+        }
         model.override_values(**overrides)
         resp, secret_ref = self.behaviors.create_secret(model)
         self.assertEqual(resp.status_code, 201)
 
         resp = self.behaviors2.get_secret(
-            secret_ref, 'application/octet-stream')
-        self.assertEqual(resp.status_code, 401)
+            secret_ref, 'application/octet-stream'
+        )
+        self.assertEqual(resp.status_code, 200)
 
     @testcase.attr('security')
     def test_cross_user_read_bogus_project_id(self):
         """Attempt to create a secret with one user, and read it with another,
         including a bogus project ID
 
-        RETURNS 200 - PROJECT ID NOT REQUIRED
-        Should return 401"""
+        Should return 200 (for now, 401 in future?)"""
 
         model = secret_models.SecretModel(**secret_create_defaults_data)
-        overrides = {'name': 'This is a super secret secret'}
+        overrides = {
+            'name': 'This is a super secret secret'
+        }
         model.override_values(**overrides)
         resp, secret_ref = self.behaviors.create_secret(model)
         self.assertEqual(resp.status_code, 201)
 
-        headers = {'X-Project-Id': bogus_project_id,
-                   'X-Auth-Token': self.client2._auth.token,
-                   'Accept': 'application/octet-stream'}
+        headers = {
+            'X-Project-Id': bogus_project_id,
+            'X-Auth-Token': self.client2._auth.token,
+            'Accept': 'application/octet-stream'
+        }
         resp = self.client2.get(
-            secret_ref, use_auth=False, extra_headers=headers)
+            secret_ref, use_auth=False, extra_headers=headers
+        )
         self.assertEqual(resp.status_code, 401)
